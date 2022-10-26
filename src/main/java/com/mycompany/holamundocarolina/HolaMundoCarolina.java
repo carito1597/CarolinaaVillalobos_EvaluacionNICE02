89eee551db60b3c18f84f915d49171fa29e966b0
Carolina Villalobos <carito15villalobos@gmail.com>
2022-10-26 17:55:47 -0400

    sort one word

--- src/main/java/com/mycompany/holamundocarolina/HolaMundoCarolina.java
+++ src/main/java/com/mycompany/holamundocarolina/HolaMundoCarolina.java
@@ -1,17 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.holamundocarolina;
 
-/**
- *
- * @author Carito
- */
+
+//Sort Inner Content
+//
+//You have to sort the inner content of every word of a string in descending order.
+//The inner content is the content of a word without first and the last char.
+//
+//Some examples:
+//"sort the inner content in descending order" -> "srot the inner ctonnet in dsnnieedcg oredr"
+//"wait for me" -> "wiat for me"
+//"this kata is easy" -> "tihs ktaa is esay"
+//"I am" -> "I am"
+
 public class HolaMundoCarolina {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(sortWord("hdcyza"));
+    }
+    
+    public static String sortWord(String word){
+        String wordSort = ""+ word.charAt(0);
+        char last = word.charAt(word.length()-1);
+        String inner = "";
+        for(int i=1; i<word.length()-1;i++){
+            inner = inner +word.charAt(i);
+        }
+        String auxiliar = sortWordInner(inner);
+        for(int j = 0; j<auxiliar.length();j++){
+            wordSort = wordSort+auxiliar.charAt(j);
+        }
+        wordSort = wordSort + last;
+        return wordSort;
+        
+    }
+   
+    public static String sortWordInner(String wordInner){
+        char wordAuxiliar[] = wordInner.toCharArray();
+        for(int i = 0; i<wordInner.length();i++){
+            for(int j = i+1; j<wordInner.length();j++){
+                if(wordAuxiliar[i]<wordAuxiliar[j]){
+                    char aux = wordAuxiliar[i];
+                    wordAuxiliar[i] = wordAuxiliar[j];
+                    wordAuxiliar[j] = aux;
+                }
+            }
+        }
+        return new String(wordAuxiliar);
     }
 }
